dd9ec86f86f9ea1735b8171ef3f5a9c256bb11f8
Valentyn Minakov <valikminak@gmail.com>
2020-07-14 09:11:49 +0300

    Updates

---
+++ precache-manifest.26de18a31b6f3dd540653c5fe009d9f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d0a9b85408f4c1ba77074f926dc2e3",
+    "url": "/PastaPizza/index.html"
+  },
+  {
+    "revision": "5bd0401ba981d5dd7a8f",
+    "url": "/PastaPizza/static/css/2.2cb6c84a.chunk.css"
+  },
+  {
+    "revision": "b43bf145e810478efc1b",
+    "url": "/PastaPizza/static/css/main.a3936eb3.chunk.css"
+  },
+  {
+    "revision": "5bd0401ba981d5dd7a8f",
+    "url": "/PastaPizza/static/js/2.a7092c4b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/PastaPizza/static/js/2.a7092c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43bf145e810478efc1b",
+    "url": "/PastaPizza/static/js/main.7771c3e0.chunk.js"
+  },
+  {
+    "revision": "3fc620a55629d9757d07",
+    "url": "/PastaPizza/static/js/runtime-main.3baa6cef.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/PastaPizza/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/PastaPizza/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/PastaPizza/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/PastaPizza/static/media/slick.f97e3bbf.svg"
+  }
+]);
